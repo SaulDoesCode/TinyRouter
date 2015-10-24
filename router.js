0129249c21ad2ff735237853d75ba2f3725582d5
Saul van der Walt <saulvdw@gmail.com>
2015-10-24 03:42:53 +0200

    new features, refactored, only 43 lines long

--- router.js
+++ router.js
@@ -1,48 +1,43 @@
 var router = {
   handle: (RouteLink, callback) => {
-    if (window.location.hash === RouteLink || window.location === RouteLink) callback();
-    router.handlers.push({
-      link: RouteLink,
-      callback: callback
-    });
+    if (location.hash === RouteLink || location === RouteLink) callback();
+    router.handlers.push({ link: RouteLink, callback: callback });
   },
   handlers: [],
-  links : [],
-  makeLink: (Selector, link, newtab,eventType) => {
-    router.links.push(() => {
-      if(eventType === undefined) eventType = 'click';
-      document.querySelector(Selector).addEventListener(eventType, e => {
-        router.openLink(link,newtab);
-      });
-    });
-  },
-  openLink : (link,newtab) => newtab ? window.open(link) : window.location = link,
-  setTitle: title => document.title = title ,
-  setView: (viewHostSelector, view) => {
-    document.body.querySelector(viewHostSelector).innerHTML = view;
-  }
-};
-
-(() => {
-  window.addEventListener('hashchange', e => {
-    router.handlers.forEach(handler => {
-      if (window.location.hash === handler.link || window.location === handler.link) {
-        handler.callback();
-      }
-    });
-  });
-  window.addEventListener('load', e => {
-    Array.from(document.querySelectorAll('[link]')).forEach((linkelement) => {
-      linkelement.addEventListener('click', e => {
-        if(linkelement.hasAttribute('newtab')) {
-           window.open(linkelement.getAttribute('link'));
-        } else {
-            window.location = linkelement.getAttribute('link');
+  links: [],
+  makeLink: (Selector, link, newtab, eventType) => router.links.push(() => document.querySelector(Selector).addEventListener((eventType === undefined ? 'click' : eventType), e => router.open(link, newtab))),
+  open : (link, newtab) => newtab ? open(link) : location = link,
+  setTitle: title => document.title = title,
+  setView: (viewHostSelector, view) => document.body.querySelector(viewHostSelector).innerHTML = view,
+  fetchView: (viewHostSelector, viewURL, cache, id) => {
+    let viewHost = document.body.querySelector(viewHostSelector);
+    if (id !== undefined && typeof id === "string" && cache === true) {
+      if (localStorage.getItem("RT_" + id) === null || localStorage.getItem("RT_" + id) === undefined) {
+        let req = new XMLHttpRequest();
+        req.onreadystatechange = e => {
+          if (req.readyState === 4 && req.status === 200) {
+            localStorage.setItem(("RT_" + id),req.response);
+            viewHost.innerHTML = req.response;
+          }
         }
-      });
-    });
-    router.links.forEach(link => {
-      link();
-    });
-  });
-})();
+        req.open('GET', viewURL);
+        req.send();
+      } else {
+        viewHost.innerHTML = localStorage.getItem("RT_" + id);
+      }
+    } else {
+      let req = new XMLHttpRequest();
+      req.onreadystatechange = e => {
+        if (req.readyState === 4 && req.status === 200) viewHost.innerHTML = req.response;
+      }
+      req.open('GET', viewURL);
+      req.send();
+    }
+  },
+  clearCache: () => { for (let i in localStorage) if (localStorage.key(i).includes("RT_")) localStorage.removeItem(localStorage.key(i)) },
+};
+window.addEventListener('hashchange', e => router.handlers.forEach(handler => (location.hash === handler.link || location === handler.link) ? handler.callback() : null));
+window.addEventListener('load', e => {
+  Array.from(document.querySelectorAll('[link]')).forEach(el => el.addEventListener('click', e => el.hasAttribute('newtab') ? open(el.getAttribute('link')) : router.open(el.getAttribute('link'))));
+  router.links.forEach(link => link());
+});
